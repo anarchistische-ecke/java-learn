1ffbbb5939eb602963c401635e585e5708160a08
freddycooper <almosttanonymous@gmail.com>
2024-02-24 00:51:09 +0300

    added slave branch with two languages

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! and how are you doiing tonight?");
+        System.out.println("здравствуй, мир!");
     }
 }
