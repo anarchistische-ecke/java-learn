e7577692d5340579789c9d057b157b18b56bcbc8
freddycooper <almosttanonymous@gmail.com>
2024-02-24 01:25:54 +0300

    just checking

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! and how are you doing tonight?");
+        System.out.println("Hello world! and how are you doing tonight??");
     }
 }
